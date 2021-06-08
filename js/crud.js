3fab6c21693497eef7c2f194b593b6d4940f49b3
michel michael <m.michel@onlineformapro.com>
2021-06-08 14:05:15 +0200

    Signed-off-by: michel michael <m.michel@onlineformapro.com> Ajout de produit http post

--- js/crud.js
+++ js/crud.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     const produits = document.querySelector('#produits');
 
+    const produitForm = document.querySelector('#ajouter-produit-form');
+    produitForm.addEventListener('submit', ajouterProduit);
+
     fetch('http://localhost:3000/produits',{
         method:"GET",
         headers:{
@@ -14,14 +17,12 @@ document.addEventListener('DOMContentLoaded', () =>{
         .then(produits  => produits.forEach(afficherProduit))
 
     function afficherProduit(produit){
-
         const produitDIV = document.createElement('div');
         produitDIV.dataset.id = produit.id
         produitDIV.id = "card-produit" + produit.id;
         produitDIV.className = 'col s3 m3';
         produitDIV.innerHTML =
-            `
-            
+            `        
                   <div class="card s3">
                     <div class="card-image">
                       <img src="${produit.imageProduit}" width="25%">
@@ -40,4 +41,34 @@ document.addEventListener('DOMContentLoaded', () =>{
        produits.appendChild(produitDIV)
 
     }
+
+    function donneeDesFormulaires(event){
+        event.preventDefault();
+        return{
+            nomProduit: `${event.target.nomProduit.value}`,
+            descriptionProduit: `${event.target.descriptionProduit.value}`,
+            prixProduit: `${event.target.prixProduit.value}`,
+            imageProduit: `${event.target.imageProduit.value}`,
+        }
+    }
+
+
+   //Fonction ajouter un produit
+   function ajouterProduit(event){
+        event.preventDefault();
+        //recup et test des valeurs des inputs name (value) du formulaire
+       let newProduit = donneeDesFormulaires(event);
+       console.log(newProduit)
+       //retouné une reqsuté de type post http
+       return fetch('http://localhost:3000/produits',{
+           method: 'POST',
+           headers:{
+               'Access-Control-Allow-Origin': '*',
+               'Content-Type': 'application/json'
+           },
+           body: JSON.stringify(newProduit)
+       })
+           .then(response => response.json())
+           .then(produit => afficherProduit(produit))
+   }
 })
