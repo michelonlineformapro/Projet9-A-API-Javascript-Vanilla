cd85f4e26af8b475f0a85dbae5bfc8be7744bd7a
michel michael <m.michel@onlineformapro.com>
2021-06-10 09:28:51 +0200

    Signed-off-by: michel michael <m.michel@onlineformapro.com> Ajout de la partie edition du produit

--- js/crud.js
+++ js/crud.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', () =>{
     //Declenche un evenement au click sur le bouton valider du formulaire ajouter
     produitForm.addEventListener('submit', ajouterProduit);
 
+    //Conteneur du formulaire d'edition
+    const updateForm = document.querySelector('#updateForm');
+
     /********************************** METHODE GET JSON LOCALHOST:3000 ****************************/
 
     //Methode fetch + url + methode GET + options
@@ -25,8 +28,9 @@ document.addEventListener('DOMContentLoaded', () =>{
         .then(produits  => produits.forEach(afficherProduit))
 
     /**************************************AFFICHER LES CARTES DE PRODUIT*************************/
+
     //Fonction ajouter produit
-    function afficherProduit(produit){
+    function afficherProduit(produit) {
         //Creer une div
         const produitDIV = document.createElement('div');
         //Ajout d'un data-set = id
@@ -54,10 +58,12 @@ document.addEventListener('DOMContentLoaded', () =>{
                   </div>`
         /*****************************AJOUTER LES CARTES DE PRODUIT********************************/
         //Ajout de la div creer comme enfant de notre conteneur HTML produits
-       produits.appendChild(produitDIV);
+        produits.appendChild(produitDIV);
+
+        /********************************************LES BOUTONS***********************************************/
 
         /*****************************************BOUTON SUPPRIMER****************************************************************/
-        //AJOUTER un bouton supprimer
+            //AJOUTER un bouton supprimer
         const btnDelete = document.createElement("button");
         //Ajouter un id unique a chaque bouton => qui match avec chaque carte
         btnDelete.setAttribute("id", `btnDelete${produit.id}`);
@@ -71,12 +77,12 @@ document.addEventListener('DOMContentLoaded', () =>{
         btnDelete.addEventListener('click', () => deleteProduit(produit));
 
         /***********************************BOUTON DETAILS PRODUIT*************************************************/
-        //Bouton details
+            //Bouton details
         const btnDetails = document.createElement('button');
         //Ajouter un attribut ID
         btnDetails.setAttribute("id", `${produit.id}`);
         //texte + id a afficher dans le bouton
-        btnDetails.innerHTML = 'Details : ' + produit.id;
+        btnDetails.innerHTML = 'Details'
         //Ajout classe css Materialize sur le bouton
         btnDetails.className = "col s4 waves-effect waves-light btn purple lighten-3";
         //Ajouter le bouton en tant qu'enfant de la div produitDIV(carte)
@@ -84,7 +90,106 @@ document.addEventListener('DOMContentLoaded', () =>{
         //Decelncher une fonction au click
         btnDetails.addEventListener('click', () => detailsProduit(produit))
 
+        /**********************************BOUTON MISE A JOUR***********************************/
+        //On creer le bouton mettre a jour pour chaque carte
+        const btnUpdate = document.createElement('button');
+        //Ajout d'un id unique a chaque bouton
+        btnUpdate.setAttribute('id', `btnUpdate${produit.id}`)
+        //Le texte du bouton
+        btnUpdate.innerHTML = 'METTRE A JOUR';
+        //Ajout classe CSS Materialize
+        btnUpdate.className = "col s4 waves-effect waves-light btn green lighten-3";
+        //Ajout du bouton comme enfant de la div carte
+        produitDIV.appendChild(btnUpdate);
+        //Au click sur le bouton on declenche une fonction
+        btnUpdate.addEventListener('click', () => editProduit(produit))
+
+    }
+        /********************************LES FONCTIONS********************************************/
+
+         /*********************************FONCTION MISE A JOUR************************************/
+         function editProduit(produit){
+             //Fomulaire ajouter produit HTML
+             const ajouterForm = document.getElementById("ajouter-produit-form");
+             //Animation = le formulaire ajouté disparait et est remplacé par le formulaire editer
+             //Effet fade opacité passe de 1 a 0 en 0.5 seconde
+             ajouterForm.animate([
+                 {opacity: 1},
+                 {opacity: 0}
+             ],{
+                 duration:500,
+             });
+             //Le formulaire ajouter sort du DOM (css display: none)
+             setTimeout(() =>{
+                 ajouterForm.style.display = "none"
+             }, 500);
+
+             //Le formulaire d'edition (on creer un formulaire + son id)
+             const editForm = document.createElement('form');
+             editForm.id = "edit-form";
+             //Ajout html du formulaire edition + value = recup des valeurs de chaque produit
+             editForm.innerHTML = `
+             <h3 class="red-text">Editer un produit</h3>
+              <div class="form-group">
+                <label>Nom du produit</label>
+                <input class="form-control" value="${produit.nomProduit}" type="text" name="nomProduit">
+              </div>
+        
+              <div class="form-group">
+                <label>Description du produit</label>
+                <input class="form-control" value="${produit.descriptionProduit}" type="text" name="descriptionProduit">
+              </div>
+        
+              <div class="form-group">
+                <label>Prix du produit</label>
+                <input class="form-control" value="${produit.prixProduit}" type="text" name="prixProduit">
+              </div>
+        
+              <div class="form-group">
+                <label>Image du produit</label>
+                <input class="form-control" type="text" value="${produit.imageProduit}" name="imageProduit">
+              </div>
+        
+              <div class="form-group">
+                <input class="col s4 waves-effect waves-light btn blue lighten-2" value="Mettre à jour" type="submit">
+                <input type="reset" value="vider les champs" class="col s4 waves-effect waves-light btn purple lighten-2">
+              </div>
+             `
+             //Le formulaire d'edition est ajouter au conteneur parent (a la ligne: 11)
+            updateForm.appendChild(editForm);
+             //On declenche une fonction a la soumission du formulaire
+             editForm.addEventListener('submit', (event) => updateProduit(event, produit))
+         }
+
+         //Traitement des nouvelles données
+         function updateProduit(event, produit){
+             //Supprime le comportement par defaut
+            event.preventDefault();
+            //Recup des valeur du formulaire grace a la methode donnéeDesFormulaire ligne: 265
+            let updatedProduit = donneeDesFormulaires();
+            //Test de debug
+            console.log(updatedProduit, produit.id);
+            //Requète HTTP PUT + url/id
+            fetch(`http://localhost:3000/produits/${produit.id}`,{
+                method:'PUT',
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify(updatedProduit)
+            })
+                //Prommesse + reponse de la requète au format json
+                .then(response => response.json())
+                .then(function (){
+                    //Debug + refresh de la page + afficher le produit modifier
+                    console.log('VOTRE PRODUIT A ETE MIS JOUR')
+                    window.location.reload()
+                    afficherProduit(produit)
+                })
+
+         }
+
         /*********************************FONCTION DETAIL PRODUIT************************************/
+
         function detailsProduit(produit){
             //Chaque carte a un id unique id=""card-produit 1 2 3 4 etc..
             const cardProduit = document.querySelector(`#card-produit${produit.id}`);
@@ -102,7 +207,9 @@ document.addEventListener('DOMContentLoaded', () =>{
                 //Appel de la methode afficher les produits ci-dessous
                 .then(produit => afficherDetailsProduit(produit))
         }
+
         /***********************************AFFICHER LES DETAILS DU PRODUIT***********************/
+
         function afficherDetailsProduit(produit){
             //Conteneur de details du produit HTML
             const detailsContainer = document.querySelector("#detailsProduit");
@@ -111,17 +218,17 @@ document.addEventListener('DOMContentLoaded', () =>{
             //Animation aves le methode animate() js + css
             produits.animate([
                 //Propriété css 1 = depart + 2 = arrivée de 0 a -300px
-                { transform: "translateX(0px)"},
-                { transform: "translateX(-300px)"}
+                { opacity: 1},
+                { opacity: 0}
             ],{
                 //Temps de la transition en 1 et 2 (de 0 a -300px en 0.5secondes)
-                duration: 200,
+                duration: 500,
             });
             //Au bout de 0.25 seconde le conteneur de carte pricipale disparait (apres la transition)
             setTimeout(() => {
                 //Propriété css display: none
                 produits.style.display = "none"
-            }, 200)
+            }, 500)
             //Valeur des details du produits
             detailsContainer.innerHTML = `
                 <div class="conatainer s12">
@@ -137,6 +244,7 @@ document.addEventListener('DOMContentLoaded', () =>{
         }
 
         /**********************************FONCTION SUPPRIMER UN PRODUIT***********************************************************************/
+
         function deleteProduit(produit){
             //recuperer id de chaque carte
             const cardProduit = document.querySelector(`#card-produit${produit.id}`)
@@ -153,22 +261,19 @@ document.addEventListener('DOMContentLoaded', () =>{
                     cardProduit.remove()
                 })
         }
-    }
-
-    /*********************************************FONCTION DETAILS DU PRODUIT*********************************/
 
     /*********************************LES DONNEES DU FORMULAIRE**************************************/
 
     //Recupéré les données du formulaire
     //Recuperation des valeurs input name des champ du formulaire comme $_POST[''] en php
-    function donneeDesFormulaires(event){
+    function donneeDesFormulaires(){
         //Supprime le comportement par defaut
         /*
         La méthode  preventDefault() de l 'interface Event indique à l'agent utilisateur que si l'événement n'est pas traité explicitement,
         son action par défaut ne doit pas être prise en compte comme elle le serait normalement. L'événement continue à se
         propager comme d'habitude, sauf si l'un de ses écouteurs appelle stopPropagation() ou stopImmediatePropagation() , dont l'un ou l'autre termine la propagation.
          */
-        event.preventDefault();
+
         return{
             nomProduit: `${event.currentTarget.nomProduit.value}`,
             descriptionProduit: `${event.target.descriptionProduit.value}`,
